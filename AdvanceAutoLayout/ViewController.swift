6dc14ea2714efd6a9e58d59bd7a8813f000f2e40
davidsantander9@gmail.com <davidsantander9@gmail.com>
2023-10-19 21:04:40 -0600

    Initial Commit

---
+++ AdvanceAutoLayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AdvanceAutoLayout
+//
+//  Created by David C Santander on 19/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
